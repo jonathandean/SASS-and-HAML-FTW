fd29f31cd330e60a84b5a9595ac3ab1e87a5cf8f
Jonathan Dean <jon@jonathandean.com>
2011-10-07 12:42:35 -0400

    Adding an empty Rails 3.1 project to get started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SASSAndHAMLFTW::Application.config.secret_token = '60925cac458bab04553fabba798710a5e399fc70cb9855a9c9267ae66c42c2d51888f682c1da0b9f1092252a0025fc9d17bbf4c03e6066a0fd29ad9730cda4aa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SASSAndHAMLFTW::Application.config.session_store :cookie_store, key: '_SASS-and-HAML-FTW_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SASSAndHAMLFTW::Application.config.session_store :active_record_store
